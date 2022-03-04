796138e90d429348a363951f70480b26f7ceb05e
IrishBruse <econn50@outlook.com>
2022-03-04 18:32:44 +0000

    Update terminal

--- Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ Documents/WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -5,32 +5,11 @@ $ENV:VISUAL = "code"
 # Remove useless ugly beep
 Set-PSReadlineOption -BellStyle None
 
-function Print {
-    [CmdletBinding()]
-    param (
-        $message,
-        $foreground,
-        $background
-    )
-    Write-Host $message -ForegroundColor $foreground -BackgroundColor $background -NoNewline
-}
-
-function Space {
-    Write-Host " " -NoNewline
-}
-
-function PrintPrompt () {
-    # Directory Printing
-    $CmdPromptCurrentFolder = Split-Path -Path $pwd -Leaf
-    Print $pwd White Black
-    Space
-}
-
 function prompt {
     # Assign Windows Title Text
-    $host.ui.RawUI.WindowTitle = "$pwd";
+    $host.ui.RawUI.WindowTitle = "Powershell - $pwd";
 
-    PrintPrompt
+    Write-Host "$pwd " -NoNewline
 
     # Print -> arrow pompt
     $Host.UI.Write($([char]0x2192))
