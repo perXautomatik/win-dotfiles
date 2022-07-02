4e59bb98a8563eb6ed927a42cefa280e0c247665
IrishBruse <Econn50@outlook.com>
2022-07-02 03:03:09 +0100

    Latest

--- boxstarter.ps1
+++ boxstarter.ps1
@@ -1,4 +1,34 @@
-Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder -EnableShowRibbon -EnableItemCheckBox
-Enable-RemoteDesktop
-Set-StartScreenOptions -EnableBootToDesktop
-Install-WindowsUpdate -AcceptEula
+Disable-InternetExplorerESC
+Disable-GameBarTips
+Disable-BingSearch
+
+Set-StartScreenOptions -EnableBootToDesktop -EnableDesktopBackgroundOnStart -EnableShowStartOnActiveScreen -EnableShowAppsViewOnStartScreen -EnableSearchEverywhereInAppsView -EnableListDesktopAppsFirst
+Set-WindowsExplorerOptions -EnableShowHiddenFilesFoldersDrives -EnableShowProtectedOSFiles -EnableShowFileExtensions -EnableShowFullPathInTitleBar -EnableOpenFileExplorerToQuickAccess -EnableShowRecentFilesInQuickAccess -EnableShowFrequentFoldersInQuickAccess -EnableExpandToOpenFolder -DisableShowRibbon -DisableItemCheckBox
+Set-BoxstarterTaskbarOptions -Size Small -Dock Bottom -Combine Never -AlwaysShowIconsOff -MultiMonitorOn -MultiMonitorMode All -DisableSearchBox
+
+choco install discord --version 1.0.9004 -y
+choco install git --version 2.37.0 -y
+choco install vscode --version 1.68.1 -y
+choco install dotnet --version 6.0.6 -y
+choco install golang --version 1.18.3 -y
+choco install 7zip --version 22.0 -y
+choco install bitwarden --version 2022.6.0 -y
+choco install ditto --version 3.24.214.0 -y
+choco install llvm --version 14.0.5 -y
+choco install rufus --version 3.18 -y
+choco install steam-client --version 2.10.91.92 -y
+choco install wiztree --version 4.08 -y
+choco install freedownloadmanager --version 6.16.2 -y
+choco install dotnet-runtime --version 6.0.6 -y
+choco install python --version 3.10.5 -y
+choco install mpc-hc --version 1.7.13.20180702 -y
+choco install alacritty --version 0.10.1 -y
+choco install rust --version 1.61.0 -y
+choco install gsudo --version 1.3.0 -y
+choco install lghub --version 2022.6.4530 -y
+choco install nodejs --version 18.4.0 -y
+choco install oraclejdk --version 17.0.2 -y
+choco install blender --version 3.2.0 -y
+choco install powertoys --version 0.59.1 -y
+choco install bulk-crap-uninstaller --version 5.3 -y
+choco install multimc --version 0.6.13 -y
--- terminal/pwsh/Microsoft.PowerShell_profile.ps1
+++ terminal/pwsh/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,3 @@
-# Modules
-. $PSScriptRoot/RemoveAlias.ps1
-
-$ENV:EDITOR = "code"
-$ENV:VISUAL = "code"
-
 function prompt {
     Write-Host "$pwd " -NoNewline
 
@@ -54,3 +48,13 @@ function copy   { Copy-Item -Path $args[0] -Destination $args[1] }
 function cp     { Copy-Item -Path $args[0] -Destination $args[1] }
 
 function ..     { cd .. }
+
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
