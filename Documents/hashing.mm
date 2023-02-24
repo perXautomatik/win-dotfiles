56324fa72cae95ed0714ef95e478d6f486d05180
perXautomatik <christoffer.broback@gmail.com>
2023-02-24 13:03:37 +0100

    In b
    (cherry picked from commit 5fad4ad567033a97e9f0995ceeb07f8ac2da4c39)
    
    # Conflicts:
    #	PowerShell

---
+++ Documents/hashing.mm
@@ -0,0 +1,14 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1673939322178" ID="ID_1315425276" MODIFIED="1673939330541" TEXT="hashing">
+<node CREATED="1673939337758" ID="ID_716225408" MODIFIED="1673939371768" POSITION="right" TEXT="Hashing while maintaining properties of initial object"/>
+<node CREATED="1673939464903" ID="ID_177622883" MODIFIED="1673939471492" POSITION="right" TEXT="hashing vs compression"/>
+<node CREATED="1673939471911" ID="ID_1801179409" MODIFIED="1673939492932" POSITION="right" TEXT="hashing vectors"/>
+<node CREATED="1673939493375" ID="ID_1315127377" MODIFIED="1673939504555" POSITION="right" TEXT="is a hash always 1 dimonsional?">
+<node CREATED="1673939523478" ID="ID_521054202" MODIFIED="1673939552682" TEXT="if the hash consists of 1 number it&apos;s then a 1 diminsional position "/>
+</node>
+<node CREATED="1673939558542" ID="ID_674821620" MODIFIED="1673939637712" POSITION="left" TEXT="is it hypotheticaly possible to generate unique hashes for any object if you mantain a certain size">
+<node CREATED="1673939644676" ID="ID_1801854276" MODIFIED="1673939655224" TEXT="git index for example and conflicts"/>
+</node>
+</node>
+</map>
