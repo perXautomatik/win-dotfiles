c73937fc117c00187cece438a9973b155682d075
perXautomatik <christoffer.broback@gmail.com>
2023-12-07 08:02:54 +0100

    reorganized, loads properly, several errors but done for now

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,4 +1,16 @@
 
+# Source scripts
+. "$PSScriptRoot/internal/env.ps1"
+. "$PSScriptRoot/internal/aliases.ps1"
+. "$PSScriptRoot/internal/functions.ps1"
+. "$PSScriptRoot/internal/autocompletions.ps1"
+
+
+Import-Module "$( $ENV:DOTFILES_DIR )/komorebi/helpers.psm1" -DisableNameChecking
+Import-Module "$( $ENV:DOTFILES_DIR )/powershell/helpers.psm1" -DisableNameChecking
+Import-Module "$( $ENV:DOTFILES_DIR )/powershell/launch.psm1" -DisableNameChecking
+	    
+
 # adds 400ms to startup time
 # Import-Module -Name Terminal-Icons
 
@@ -10,28 +22,16 @@ Import-Module-Verified "posh-git"
 Load-Module "posh-git"
 
 # adds 400ms to startup time
-Import-Module-Verified "Terminal-Icons"
-Load-Module "Terminal-Icons"
+Import-Module-Verified "Terminal-Icons" #Load-Module "Terminal-Icons"
 
-Import-Module-Verified "cd-extras"
-Load-Module "cd-extras"  
+Import-Module-Verified "cd-extras" #Load-Module "cd-extras"  
 
 Load-Module "PSReadLine"
 
-# Source scripts
-. "$PSScriptRoot/internal/env.ps1"
-. "$PSScriptRoot/internal/aliases.ps1"
-. "$PSScriptRoot/internal/functions.ps1"
-. "$PSScriptRoot/internal/autocompletions.ps1"
 . "$PSScriptRoot/internal/loads.ps1"
 . "$PSScriptRoot/internal/key-config.ps1"
 . "$HOME\.config\powershell\lf_icons.ps1"
 
-
-Import-Module "$( $ENV:DOTFILES_DIR )/komorebi/helpers.psm1" -DisableNameChecking
-Import-Module "$( $ENV:DOTFILES_DIR )/powershell/helpers.psm1" -DisableNameChecking
-Import-Module "$( $ENV:DOTFILES_DIR )/powershell/launch.psm1" -DisableNameChecking
-	    
 
 # Encoding
 [console]::InputEncoding = [console]::OutputEncoding = New-Object System.Text.UTF8Encoding
